67a0ec1d61ff71a77ad3f33d8fa634a78a32d946
Victor Kamau <victormungai97@gmail.com>
2025-11-16 01:27:11 +0300

    build: :wrench: Configurations
    Updated code configurations for the project

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  cacheComponents: true,
+  cacheLife: {
+    twoweeks: {
+      stale: 60 * 5,
+      revalidate: 60 * 60 * 24 * 14,
+      expire: 60 * 60 * 24 * 365,
+    },
+  },
 };
 
 export default nextConfig;
